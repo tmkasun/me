59c8c62cb24add9e631ac3b7d01f829dc3d6329b
Kasun Thennakoon <tmkasun@gmail.com>
2020-05-10 15:24:45 +0530

    temopory remove 404

---
+++ src/pages/_404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const F04 = (props) => {
+    return (
+        <div>
+            <h3>
+                Opps! Can not find the knnection !!
+            </h3>
+        </div>
+    )
+}
+
+export default F04
