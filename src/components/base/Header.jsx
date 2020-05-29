ffc101c7c0888fa6f1a35cf9c231069716535740
Kasun Thennakoon <tmkasun@gmail.com>
2020-05-29 09:16:19 +0530

    update header

--- src/components/base/Header.jsx
+++ src/components/base/Header.jsx
@@ -6,6 +6,7 @@ import Grid from "@material-ui/core/Grid"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Button from '@material-ui/core/Button';
 import { Helmet } from "react-helmet";
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 import IconButton from "@material-ui/core/IconButton"
 import NightsStayIcon from "@material-ui/icons/NightsStay"
@@ -26,9 +27,25 @@ const useStyles = makeStyles(theme => ({
     color: theme.palette.type === 'light' ? lightBlue[600] : cyan[200]
   },
   appBarPaperRoot: {
-    backgroundColor: theme.palette.background.default
+    backdropFilter: 'blur(4px)',
+    backgroundColor: `${theme.palette.background.default}04`
   }
 }))
+
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 2 : 0,
+  });
+}
 
 /**
  * My web page specific Header component composed using Helmet and Material-UI Appbar
@@ -64,56 +81,58 @@ export default function Header(props) {
         <meta charSet="utf-8" />
         <meta name="description" content={description || site.siteMetadata.description} />
       </Helmet>
-      <AppBar
-        elevation={0}
-        classes={{ root: classes.appBarPaperRoot }}
-        color="inherit"
-        position="static"
-      >
-        <Toolbar>
-          <Grid
-            container
-            direction="row"
-            justify="flex-end"
-            alignItems="center"
-            spacing={2}
-          >
-            <Grid item>
-              <Link activeStyle={{ color: "read" }} to="/">
-                <Button aria-label="Home" className={classes.links}>
-                  Home
+      <ElevationScroll {...props}>
+        <AppBar
+          elevation={0}
+          classes={{ root: classes.appBarPaperRoot }}
+          color="inherit"
+        >
+          <Toolbar>
+            <Grid
+              container
+              direction="row"
+              justify="flex-end"
+              alignItems="center"
+              spacing={2}
+            >
+              <Grid item>
+                <Link activeStyle={{ color: "read" }} to="/">
+                  <Button aria-label="Home" className={classes.links}>
+                    Home
                     </Button>
-              </Link>
-            </Grid>
+                </Link>
+              </Grid>
 
-            <Grid item>
-              <Link activeStyle={{ color: "read" }} to="/blog">
-                <Button aria-label="Blog" className={classes.links}>
-                  Blog
+              <Grid item>
+                <Link activeStyle={{ color: "read" }} to="/blog">
+                  <Button aria-label="Blog" className={classes.links}>
+                    Blog
                     </Button>
-              </Link>
-            </Grid>
+                </Link>
+              </Grid>
 
-            <Grid item>
-              <Link activeStyle={{ color: "read" }} to="/projects">
-                <Button aria-label="Projects" className={classes.links}>
-                  Projects
+              <Grid item>
+                <Link activeStyle={{ color: "read" }} to="/projects">
+                  <Button aria-label="Projects" className={classes.links}>
+                    Projects
                     </Button>
-              </Link>
-            </Grid>
+                </Link>
+              </Grid>
 
-            <Grid item>
-              <IconButton aria-label="Dark & Light mode" onClick={oClick} size="small">
-                {isDarkMode ? (
-                  <WbSunnyIcon className={classes.sunLight} fontSize="small" />
-                ) : (
-                    <NightsStayIcon style={{ color: 'black' }} fontSize="small" />
-                  )}
-              </IconButton>
+              <Grid item>
+                <IconButton aria-label="Dark & Light mode" onClick={oClick} size="small">
+                  {isDarkMode ? (
+                    <WbSunnyIcon className={classes.sunLight} fontSize="small" />
+                  ) : (
+                      <NightsStayIcon style={{ color: 'black' }} fontSize="small" />
+                    )}
+                </IconButton>
+              </Grid>
             </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
     </div>
   )
 }
