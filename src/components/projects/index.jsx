f927b0b075cff64f48335bf8687d57b6d9e03f2b
Kasun Thennakoon <tmkasun@gmail.com>
2020-06-08 23:58:28 +0530

    Add styling to projects

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,34 +1,17 @@
 import React, { useState } from "react"
 import Grid from "@material-ui/core/Grid"
-import Divider from "@material-ui/core/Divider"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
+import Link from "../base/KLink"
 
 import Card from "@material-ui/core/Card"
-import CardActionArea from "@material-ui/core/CardActionArea"
-import CardActions from "@material-ui/core/CardActions"
 import CardContent from "@material-ui/core/CardContent"
-import Button from "@material-ui/core/Button"
 
 const useStyles = makeStyles(theme => ({
-  container: {
-    height: "100%",
-  },
-  appContent: {
-    marginTop: theme.spacing(2),
-    maxWidth: "95%",
-    margin: "auto",
-    maxHeight: theme.spacing(90),
-    height: theme.spacing(90),
-    overflow: "scroll",
-  },
   card: {
-    "& a": {
-      color: "inherit",
-    },
-    maxWidth: 270,
     minHeight: 250,
     paddingBottom: 20,
+    overflow: "scroll"
   },
   media: {
     height: 220,
@@ -37,8 +20,8 @@ const useStyles = makeStyles(theme => ({
 export default props => {
   const classes = useStyles()
   const [apps] = useState([
-    { name: 'Personal Web', description: 'This personal web portal was developed from the scratch inspired by `overreacted` `addyosmani` and many other great personal web portals', url: 'https://github.com/tmkasun/me' },
-    { name: 'Git Stats', description: 'Show PR status', url: 'https://csb-vrskp.netlify.app/' },
+    { name: 'Personal Web', description: 'I have developed this personal web portal to match with my custom requirements and was inspired by `overreacted` `addyosmani` and many other great personal web portals', url: 'https://github.com/tmkasun/me' },
+    { name: 'Git Stats', description: 'Show PR status of pre-defined github filtering query using Github REST APIs', url: 'https://csb-vrskp.netlify.app/' },
     { name: 'Home Server', description: 'Knnect Home server', url: '/home' },
     { name: 'C++ Basics', description: 'How C++ (AKA CPP) programs works', url: 'https://github.com/tmkasun/understanding_cpp' },
     { name: 'Streaming Graph partitioning', description: 'Simulate how Graph partitioning works', url: 'https://github.com/tmkasun/streaming_graph_partitioning' },
@@ -49,67 +32,38 @@ export default props => {
   ])
 
   return (
-    <Grid container direction="row" justify="center" alignItems="center">
-      <Grid item md={6}>
-        <Typography align="center" variant="h5">
-          Applications
-        </Typography>
-        <Divider />
-        <Grid
-          container
-          spacing={0}
-          justify="center"
-          className={classes.container}
-        >
-          <Grid item xs={12}>
-            <div className={classes.appContent}>
-              <Grid
-                style={{ padding: "10px" }}
-                spacing={1}
-                container
-                direction="row"
-                justify="flex-start"
-                alignItems="center"
-              >
-                {apps &&
-                  apps.map(app => (
-                    <Grid item xs={3}>
-                      <Card className={classes.card}>
-                        <CardActionArea>
-                          <a href={app.url}>
-                            <CardContent className={classes.media}>
-                              <Typography
-                                gutterBottom
-                                variant="h5"
-                                component="h2"
-                              >
-                                {app.name}
-                              </Typography>
-                              <Typography
-                                variant="body2"
-                                color="textSecondary"
-                                component="p"
-                              >
-                                {app.description}
-                              </Typography>
-                            </CardContent>
-                          </a>
-                        </CardActionArea>
-                        <CardActions>
-                          <a href={app.url}>
-                            <Button aria-label="Edit" variant="outlined" color="primary">
-                              Link
-                            </Button>
-                          </a>
-                        </CardActions>
-                      </Card>
-                    </Grid>
-                  ))}
-              </Grid>
-            </div>
+    <Grid
+      style={{ padding: "10px" }}
+      spacing={2}
+      container
+      direction="row"
+      justify="space-evenly"
+      alignItems="center"
+    >
+      {apps &&
+        apps.map(app => (
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <Card raised elevation={3} className={classes.card}>
+              <Link href={app.url}>
+                <CardContent className={classes.media}>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="h2"
+                  >
+                    {app.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                  >
+                    {app.description}
+                  </Typography>
+                </CardContent>
+              </Link>
+            </Card>
           </Grid>
-        </Grid>
-      </Grid>
+        ))}
     </Grid>
   )
 }
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,11 +1,28 @@
 import React from "react"
 import Base from "../../components/base/index"
 import Projects from "../../components/projects/index"
+import MyTitle from "../../components/landing/MyTitle"
+import Grid from "@material-ui/core/Grid"
+import Divider from "@material-ui/core/Divider"
 
 export default props => {
   return (
-    <Base>
-      <Projects />
+    <Base headerProps={{title: "Kasun Thennakoon's personal projects"}}>
+       <Grid
+        container
+        spacing={0}
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item md={3} sm={1} xs={false} />
+        <Grid item md={6} sm={10} xs={12}>
+          <MyTitle>My personal projects</MyTitle>
+          <Divider />
+          <Projects />
+        </Grid>
+        <Grid item md={3} sm={1} xs={false} />
+      </Grid>
     </Base>
   )
 }
